f844fc97d61dfde2f70ce636489696fe6600a397
derekwilfong <dkwilfong@gmail.com>
2016-08-11 12:17:54 -0700

    Update Class1.cs
    Cleaned-up code; added transaction

--- CreateLevels/CreateLevels/Class1.cs
+++ CreateLevels/CreateLevels/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,102 +15,32 @@ using Autodesk.Revit.UI.Selection;
 
 namespace CreateLevels
 {
+
     [Transaction(TransactionMode.Manual)]
 
     class Command : IExternalCommand
     {
-        //private object document;
-
-        public Result Execute(
-            ExternalCommandData commandData,
-            ref string message,
-            ElementSet elements)
-
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            {
 
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("MyTransaction");
                 Level level = doc.Create.NewLevel(0);
                 if (null == level)
-                {
-                    throw new Exception("Create new level failed.");
-                }
-                level.Name = "New Name";
+
+                    level.Name = "New Name";
                 ElementId nid = level.Id;
 
-                return level;
-
+                tx.Commit();
             }
-
+            throw new Exception("Create new level failed.");
+            //throw new NotImplementedException();
         }
-
     }
-
 }
-
-
-
-
-
-    
-
-        
-
-
-       
-        
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //    {
-    //        UIApplication uiapp = commandData.Application;
-    //        UIDocument uidoc = uiapp.ActiveUIDocument;
-    //        Application app = uiapp.Application;
-    //        Document doc = uidoc.Document;
-
-//        using (Transaction trans = new Transaction(doc, "Level"))
-//        {
-
-//            trans.Start();
-
-
-//            {
-//                double elevation = 20.0;
-
-//                Level level = doc.Create.NewLevel(elevation);
-//                if (null == level)
-//                {
-//                    throw new Exception("Create a new level failed.");
-//                }
-//                // change the leve name
-//                level.Name = "New Level";
-
-//                return Result.Succeeded;
-
-//                trans.Commit();
-//            }
-//        }
-
-//    }
-//}
-
-
-
-
